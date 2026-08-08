8753b9c0ae544ee4234ca651728b65a33190036b
alad80 <125597187+alad80@users.noreply.github.com>
2026-08-08 14:20:20 +0300

    Update gold_bot.py

--- gold_bot.py
+++ gold_bot.py
@@ -1,6 +1,4 @@
-from signal_writer import write_signal
-
 def send_trend_message():
     message = "Trend: Yükseliş"
-    send_message(message)      
-    write_signal("YUKSELIS")   
+
+    send_message(message)
